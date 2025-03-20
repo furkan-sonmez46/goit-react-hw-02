a617d823a3842fcf714ff271a6751e85fd428021
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-03-21 01:07:59 +0300

    components has done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import Description from '../Description/Description.jsx';
+import '../Description/Description.css';
+import Options from '../Options/Options.jsx';
+import '../Options/Options.css';
+import Feedback from '../Feedback/Feedback.jsx';
+import '../Feedback/Feedback.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem('feedback');
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
+  });
+
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = feedbackType => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Description />
+      <Options
+        feedback={feedback}
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+      />
+      <Feedback feedback={feedback} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,17 @@
+const Feedback = ({ feedback }) => {
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const forPercent = feedback.good + feedback.bad;
+  if (!feedback.good && !feedback.neutral && !feedback.bad) {
+    return <p>No feedback yet</p>;
+  }
+  return (
+    <div>
+      <p>Good: {feedback.good}</p>
+      <p>Neutral: {feedback.neutral}</p>
+      <p>Bad: {feedback.bad}</p>
+      <p>Total: {totalFeedback}</p>
+      <p>Positive: {Math.round((feedback.good / forPercent) * 100)}%</p>
+    </div>
+  );
+};
+export default Feedback;
