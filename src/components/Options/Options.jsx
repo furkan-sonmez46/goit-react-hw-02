4756daf5cc1b8e1886daebfb86fb9a76915ea266
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-03-21 01:29:40 +0300

    options modified

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,12 +1,29 @@
+import { useState } from 'react';
+import styles from './Options.module.css';
+
 const Options = ({ feedback, updateFeedback, resetFeedback }) => {
+  const [bgClass, setBgClass] = useState(styles.default);
+
+  const handleClick = type => {
+    updateFeedback(type);
+    if (type === 'good') setBgClass(styles.good);
+    else if (type === 'neutral') setBgClass(styles.neutral);
+    else if (type === 'bad') setBgClass(styles.bad);
+  };
+
+  const handleReset = () => {
+    resetFeedback();
+    setBgClass(styles.default);
+  };
+
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
   return (
-    <div>
-      <button onClick={() => updateFeedback('good')}>Good</button>
-      <button onClick={() => updateFeedback('neutral')}>Neutral</button>
-      <button onClick={() => updateFeedback('bad')}>Bad</button>
-      {totalFeedback > 0 && <button onClick={resetFeedback}>Reset</button>}
+    <div className={`${styles.container} ${bgClass}`}>
+      <button onClick={() => handleClick('good')}>Good</button>
+      <button onClick={() => handleClick('neutral')}>Neutral</button>
+      <button onClick={() => handleClick('bad')}>Bad</button>
+      {totalFeedback > 0 && <button onClick={handleReset}>Reset</button>}
     </div>
   );
 };
